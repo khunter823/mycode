60f9546b3867309255c781b1f1658a75cda2f080
khunter823 <khunter823@users.noreply.github.com>
2022-04-19 23:02:24 +0000

    writing custom code

---
+++ custif/project1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+user_action = input("Enter a choice (rock, paper, scissors): ")
+possible_action = ["rock", "paper", "scissors"]
+computer_action = random.choice(possible_actions)
+
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+if user_action == computer_action:
+    print(f"Both players selected {user_action}, It's a tie!")
